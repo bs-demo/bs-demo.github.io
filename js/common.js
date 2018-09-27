c575366139d9223d2a377902d0281a23cbf578c4
Sapargal13v <sapargal13v@mail.ru>
2018-09-27 12:37:17 +0600

    11
    11

--- js/common.js
+++ js/common.js
@@ -1,8 +1,46 @@
-$(document).ready(function () {
-    $(".my-row").owlCarousel({
-        items: 4,
-        navigation: true,
-        pagination: true,
-        margin: 4
-    });
+$(function() {
+  
+  $("#toggle").click(function() {
+    $(this).toggleClass("on");
+    $("#resize").toggleClass("active");
+  });
+  
 });
+
+var didScroll;
+var lastScrollTop = 0;
+var delta = 5;
+var navbarHeight = $('body').outerHeight();
+
+$(window).scroll(function(event){
+    didScroll = true;
+});
+
+setInterval(function() {
+    if (didScroll) {
+        hasScrolled();
+        didScroll = false;
+    }
+}, 250);
+
+function hasScrolled() {
+    var st = $(this).scrollTop();
+    
+    // Make sure they scroll more than delta
+    if(Math.abs(lastScrollTop - st) <= delta)
+        return;
+    
+    // If they scrolled down and are past the navbar, add class .nav-up.
+    // This is necessary so you never see what is "behind" the navbar.
+    if (st > lastScrollTop && st > navbarHeight){
+        // Scroll Down
+        $('body').removeClass('main-menu').addClass('nav-up');
+    } else {
+        // Scroll Up
+        if(st + $(window).height() < $(document).height()) {
+            $('body').removeClass('nav-up').addClass('menu-menu');
+        }
+    }
+    
+    lastScrollTop = st;
+}
